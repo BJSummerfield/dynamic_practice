d671799fe3c36f4ec30fc060ff257b9e06935001
Brian Summerfield <brian@Brians-Air.lan>
2021-07-15 22:08:43 -0500

    add isbalanced

---
+++ isBalanced.mjs
@@ -0,0 +1,26 @@
+import Tests from './tests/isBalanced_tests.mjs';
+
+const tests = new Tests
+
+const isbalanced = (string) => {
+  const stack = [];
+  const brackets = {
+    "(":")",
+    "{":"}",
+    "[":"]",
+    "<":">"
+  };
+
+  for (let char of string) {
+    if ( char in brackets ) stack.push(char);
+    if ( Object.values(brackets ).includes(char)) {
+      if ( brackets[stack.pop()] !== char ) return false
+    };
+  };
+
+  return !stack.length;
+};
+
+for (let test of tests.cases) {
+  console.log(isbalanced(test));
+};
