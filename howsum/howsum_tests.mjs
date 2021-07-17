c195805f3f8d2db8f9e9f7256f0611bf79c2859d
Brian Summerfield <brian@Brians-Air.lan>
2021-07-17 14:58:03 -0500

    howsum Tab complete

---
+++ howsum/howsum_tests.mjs
@@ -0,0 +1,13 @@
+class Tests {
+  constructor(){
+    this.cases = [
+      [8,[3,3,2,4]],
+      [13,[6,9,10]],
+      [20,[2,3,4]],
+      [30,[18,5,9,7]],
+      [100,[32,40,25]]
+    ]
+  }
+}
+
+export default Tests
