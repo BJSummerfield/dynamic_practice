c2131796b495562d00035dd26af9e5487d2d3a38
Brian Summerfield <brianjsummerfield@gmail.com>
2021-07-26 13:47:54 -0500

    countconstruct_tab complete

--- countconstruct/countconstruct_rec.mjs
+++ countconstruct/countconstruct_rec.mjs
@@ -2,15 +2,15 @@ import Tests from './countconstruct_tests.mjs'
 
 const tests = new Tests
 
-const countConstruct = (target, wordbank, memo = {}) => {
+const countConstruct = (target, wordBank, memo = {}) => {
   if (target in memo) return memo[target];
   if (target === "") return 1;
 
   let totalways = 0;
 
-  for (let word of wordbank) {
+  for (let word of wordBank) {
     if (target.indexOf(word) === 0) {
-      totalways += countConstruct(target.slice(word.length), wordbank, memo);
+      totalways += countConstruct(target.slice(word.length), wordBank, memo);
       memo[target] = totalways;
     }
   }
