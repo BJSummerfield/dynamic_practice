f71e26553a8469e36e576e1d1420c5d00b5bfb9b
Brian Summerfield <brianjsummerfield@gmail.com>
2021-07-28 09:14:36 -0500

    edit test

--- canconstruct/canconstruct_tests.mjs
+++ canconstruct/canconstruct_tests.mjs
@@ -1,9 +1,9 @@
 class Tests {
   constructor(){
     this.cases = [
-      ['abcdef',['ab','abc','cd','def','abcd']],
+      ['abcdef',['ab','abc','cd','def','abcd','ef','c']],
       ['skateboard',['bo','rd','ate','t','ska','sk','boar']],
-      ['arielpayne',['a','ari','pay','el','yne','ne']],
+      ['arielpayne',['a','ari','pay','el','yne','ne','ri']],
       ['eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeez',['e','ee','eee','eeee']],
     ]
   };
