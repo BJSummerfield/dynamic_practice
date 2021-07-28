285625312c9a5b9f6203fb9a2977ee3f19e5801e
Brian Summerfield <brianjsummerfield@gmail.com>
2021-07-28 08:34:25 -0500

    tests written

---
+++ allconstruct/allconstruct_tests.mjs
@@ -0,0 +1,12 @@
+class Tests {
+  constructor(){
+    this.cases = [
+      ['abcdef',['ab','abc','cd','def','abcd']],
+      ['skateboard',['bo','rd','ate','t','ska','sk','boar']],
+      ['arielpayne',['a','ari','pay','el','yne','ne']],
+      ['eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeez',['e','ee','eee','eeee']],
+    ]
+  };
+};
+
+export default Tests
